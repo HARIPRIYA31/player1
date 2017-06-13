5332f58c179b9b314bc010045e529b17d7718f15
HARIPRIYA31 <priyaravichandran1997@gmail.com>
2017-06-13 10:30:32 +0530

    Create reverse_str.java

--- reverse_str.java
+++ reverse_str.java
@@ -5,6 +5,7 @@ public class reverse_str
    public static void main(String args[])
    {
       Scanner sc=new Scanner(System.in);
+      System.out.println("ENTER A STRING:");
       String str=sc.nextLine();
       String ans=new StringBuffer(str).reverse().toString();
       System.out.println("ORIGINAL:"+str);
